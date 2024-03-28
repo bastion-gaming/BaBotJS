d66768b168b13e3904707c61e06489b2d9f8a234
Azerxim <mallet.maxime@hotmail.fr>
2024-03-28 16:26:15 +0100

    fix

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -6,13 +6,13 @@ module.exports = {
 	name: 'messageCreate',
 	async execute(message) {
 		let PREFIX_CHECK = false;
-		for (const prefix of ge.prefixs) {
-			if (message.content.indexOf(prefix) == 0) {
-				PREFIX_CHECK = true;
+		try {
+			for (const prefix of ge.prefixs) {
+				if (message.content.indexOf(prefix) == 0) {
+					PREFIX_CHECK = true;
+				}
 			}
-		}
-		if (!PREFIX_CHECK) {
-			try {
+			if (!PREFIX_CHECK) {
 				if (ge.guildID.includes(String(message.guild.id))) {
 					await lvl.xpmsg(message);
 					await lvl.checklevel(message);
@@ -21,9 +21,9 @@ module.exports = {
 					await stats.add('msg');
 				}
 			}
-			catch (error) {
-				console.log(`${ge.bcolors.redb}${error}${ge.bcolors.end}`);
-			}
+		}
+		catch (error) {
+			console.log(`${ge.bcolors.redb}${error}${ge.bcolors.end}`);
 		}
 	},
 };
